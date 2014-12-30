ddc3c4ef7d16f5371320f3ae5ce9381a498201d9
Ty Lyon <tyklyon@gmail.com>
2014-12-30 11:49:36 -0500

    Update visitor_can_view_reviews_spec.rb
    shortened line 7

--- spec/features/visitor_can_view_reviews_spec.rb
+++ spec/features/visitor_can_view_reviews_spec.rb
@@ -4,7 +4,12 @@ feature "Visitor can see reviews of a station" do
 
   it "can view a station's reviews from the station's view page" do
     station = FactoryGirl.create(:station)
-    review = Review.create(description: "gross", station_id: station.id, user_id: 1, rating: 4 )
+    review = Review.create(
+      description: "gross", 
+      station_id: station.id, 
+      user_id: 1, 
+      rating: 4 
+    )
     visit station_path(station.id)
     expect(page).to have_content station.address
     expect(page).to have_content review.description
