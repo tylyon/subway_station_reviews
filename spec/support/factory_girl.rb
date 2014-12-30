aa1c697d88532819be161c3d538e5c134175875a
Ty Lyon <tyklyon@gmail.com>
2014-12-30 11:51:35 -0500

    Update factory_girl.rb
     double-quoted strings

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -3,14 +3,14 @@ require "factory_girl"
 FactoryGirl.define do
   factory :user do
     sequence(:email) {|n| "user#{n}@example.com" }
-    password 'password'
-    password_confirmation 'password'
+    password "password"
+    password_confirmation "password"
   end
 
   factory :station do
-    name 'name'
-    line_id '3'
-    address '123 fake st.'
+    name "name"
+    line_id 3
+    address "123 fake st."
   end
   
 end
