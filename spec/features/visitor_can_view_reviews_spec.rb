65b517612716b02312897679e51497648d0ad144
Ty Lyon <tyklyon@gmail.com>
2014-12-30 11:43:24 -0500

    Update visitor_can_view_reviews_spec.rb
    removed "save_and_open_page" debugging statement

--- spec/features/visitor_can_view_reviews_spec.rb
+++ spec/features/visitor_can_view_reviews_spec.rb
@@ -7,7 +7,6 @@ feature "Visitor can see reviews of a station" do
     review = Review.create(description: "gross", station_id: station.id, user_id: 1, rating: 4 )
     visit station_path(station.id)
     expect(page).to have_content station.address
-    save_and_open_page
     expect(page).to have_content review.description
     expect(page).to have_content review.rating
   end
