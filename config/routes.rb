2c46d1bcc81bd98935016a57181e960c1ec9eb2b
Chris Wand <chris.wand@gmail.com>
2014-12-31 11:18:58 -0500

    style cleanup per hound

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   root "stations#index"
   devise_for :users
 
-  resources :stations, only: [:index, :show, :new, :create, :edit, :update,
-    :destroy]
+  resources :stations, only: [
+    :index,
+    :show,
+    :new,
+    :create,
+    :edit,
+    :update,
+    :destroy
+  ]
 end
