96e6a8468ab8613825159215a10e45588d84d434
Chris Wand <chris.wand@gmail.com>
2014-12-31 11:15:21 -0500

    Delete stations functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root "stations#index"
   devise_for :users
 
-  resources :stations, only: [:index, :show, :new, :create, :edit, :update]
+  resources :stations, only: [:index, :show, :new, :create, :edit, :update,
+    :destroy]
 end
