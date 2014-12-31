25910c1781e5d15919669e1a8dd8487f4d32566d
Ty Lyon <tyklyon@gmail.com>
2014-12-31 15:52:32 -0500

    Update user_edits_review_spec.rb

--- spec/features/user_edits_review_spec.rb
+++ spec/features/user_edits_review_spec.rb
@@ -6,41 +6,44 @@ feature "user edits review", %{
   So I can update my review if something changes
 } do
     before(:each) do
-        user = FactoryGirl.create(:user)
-        review = FactoryGirl.create(:review)
-        station = FactoryGirl.create(:station)
-        review = Review.new(description: review.description, rating: review.rating, station_id: station.id, user_id: user.id)
-        review.save
+      user = FactoryGirl.create(:user)
+      review = FactoryGirl.create(:review)
+      station = FactoryGirl.create(:station)
+      review = Review.new(description: review.description, 
+      rating: review.rating, 
+      station_id: station.id, 
+      user_id: user.id)
+      review.save
+      
+      visit new_user_session_path
+      
+      fill_in "Email", with: user.email
+      fill_in "Password", with: user.password
 
-        visit new_user_session_path
+      click_button "Log in"
 
-        fill_in "Email", with: user.email
-        fill_in "Password", with: user.password
-
-        click_button "Log in"
-
-        visit station_path(station)
-        click_link "Edit"
+      visit station_path(station)
+      click_link "Edit"
     end
 
     scenario "Editing a review" do
-        fill_in "Description", with: "Great place to live and work"
-        select 4, from: "Rating"
+      fill_in "Description", with: "Great place to live and work"
+      select 4, from: "Rating"
 
-        click_button "Update"
+      click_button "Update"
 
-        expect(page).to have_content("Great place to live and work")
-        expect(page).to have_content("4")
-        expect(page).to have_content("Review edited")
+      expect(page).to have_content("Great place to live and work")
+      expect(page).to have_content("4")
+      expect(page).to have_content("Review edited")
     end
 
     scenario "User cannot submit empty edit" do
-        fill_in "Description", with: ""
-        select 4, from: "Rating"
+      fill_in "Description", with: ""
+      select 4, from: "Rating"
 
-        click_button "Update"
-
-        expect(page).to have_content("4")
-        expect(page).to have_content("Description can't be blank")
+      click_button "Update"
+        
+      expect(page).to have_content("4")
+      expect(page).to have_content("Description can't be blank")
     end
 end
