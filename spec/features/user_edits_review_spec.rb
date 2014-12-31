445031db55c18e83836bd67d9341c2c498b3bc44
Ty Lyon <tyklyon@gmail.com>
2014-12-31 15:50:23 -0500

    Update user_edits_review_spec.rb
    single-quotes to double-quotes

--- spec/features/user_edits_review_spec.rb
+++ spec/features/user_edits_review_spec.rb
@@ -1,6 +1,6 @@
-require 'rails_helper'
+require "rails_helper"
 
-feature 'user edits review', %Q{
+feature "user edits review", %{
   As a signed in user
   I want to edit my review
   So I can update my review if something changes
@@ -14,33 +14,33 @@ feature 'user edits review', %Q{
 
         visit new_user_session_path
 
-        fill_in 'Email', with: user.email
-        fill_in 'Password', with: user.password
+        fill_in "Email", with: user.email
+        fill_in "Password", with: user.password
 
-        click_button 'Log in'
+        click_button "Log in"
 
         visit station_path(station)
-        click_link 'Edit'
+        click_link "Edit"
     end
 
-    scenario 'Editing a review' do
-        fill_in 'Description', with: "Great place to live and work"
-        select 4, from: 'Rating'
+    scenario "Editing a review" do
+        fill_in "Description", with: "Great place to live and work"
+        select 4, from: "Rating"
 
-        click_button 'Update'
+        click_button "Update"
 
-        expect(page).to have_content('Great place to live and work')
-        expect(page).to have_content('4')
-        expect(page).to have_content('Review edited')
+        expect(page).to have_content("Great place to live and work")
+        expect(page).to have_content("4")
+        expect(page).to have_content("Review edited")
     end
 
-    scenario 'User cannot submit empty edit' do
-        fill_in 'Description', with: ""
-        select 4, from: 'Rating'
+    scenario "User cannot submit empty edit" do
+        fill_in "Description", with: ""
+        select 4, from: "Rating"
 
-        click_button 'Update'
+        click_button "Update"
 
-        expect(page).to have_content('4')
+        expect(page).to have_content("4")
         expect(page).to have_content("Description can't be blank")
     end
 end
