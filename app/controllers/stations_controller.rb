c20ecc391705b14c852043333ade4ebc188368e2
Chris Wand <chris.wand@gmail.com>
2015-01-05 10:25:06 -0500

    Clean up superfluous functions in stations_controller

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -9,25 +9,4 @@ class StationsController < ApplicationController
     @reviews = @station.reviews
     @review = Review.new
   end
-
-  def new
-    @station = Station.new
-  end
-
-  def create
-    @station = Station.new(station_params)
-    if @station.save
-      flash[:notice] = "Station created."
-      redirect_to station_path(@station)
-    else
-      @errors = @station.errors.full_messages
-      render new_station_path
-    end
-  end
-
-  private
-
-  def station_params
-    params.require(:station).permit(:name, :address)
-  end
 end
