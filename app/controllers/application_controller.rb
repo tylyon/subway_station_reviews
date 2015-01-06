e84a779308f7b90d1fac07138eb5aa83e6090541
Chris Wand <chris.wand@gmail.com>
2015-01-06 14:31:30 -0500

    fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,16 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def authenticate_admin
+    unless current_user && current_user.role == "admin"
+      flash[:notice] = "Only an admin can do that"
+      redirect_to root_path
+      return false
+    end
+    true
+  end
+
+
   def authenticate_review(review)
     if current_user.id != review.user_id
       flash[:notice] = "Only the owner for this review can do that"
