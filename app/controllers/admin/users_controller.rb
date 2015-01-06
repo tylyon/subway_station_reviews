3252d88cf35cda42b08118bffcf1a817bc9da139
Chris Wand <chris.wand@gmail.com>
2015-01-06 10:15:32 -0500

    Added test and feature for admins to give admin privileges to other users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -14,4 +14,15 @@ class Admin::UsersController < ApplicationController
       redirect_to admin_users_path
     end
   end
+
+  def update
+    @user = User.find(params[:id])
+    if !authenticate_admin
+      return
+    else
+      @user.update(role: "admin")
+      flash[:notice] = "User #{@user.email} granted admin privileges"
+      redirect_to admin_users_path
+    end
+  end
 end
