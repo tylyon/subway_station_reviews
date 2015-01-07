80c3864f38d7b2c7a4c16979a199ac7b7e4cf7dd
jpaskett <paskett.3@osu.edu>
2015-01-07 15:25:55 -0500

    Saves branch

--- spec/features/user_votes_spec.rb
+++ spec/features/user_votes_spec.rb
@@ -15,7 +15,6 @@ feature "User Votes ", %Q{
       @review = FactoryGirl.create(:review)
 
       visit new_user_session_path
-
       fill_in "Email", with: user.email
       fill_in "Password", with: user.password
 
