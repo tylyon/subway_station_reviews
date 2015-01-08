568df824e54fa46d5bc7957996dbc1f3d1a6dd9f
Chris Wand <chris.wand@gmail.com>
2015-01-08 17:03:59 -0500

    Layout and fixes to semantic.css

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,6 +4,7 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( semantic.min.js )
 Rails.application.config.assets.precompile += %w( semantic.min.css )
+Rails.application.config.assets.precompile += %w( semantic.css )
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
